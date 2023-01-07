00c2000e06214ccafa9877b9c443b8d690899863
PavelShlyapnikov <thevanroth@gmail.com>
2023-01-07 15:48:18 +0500

    + коммент

--- src/lr2/Example_2.java
+++ src/lr2/Example_2.java
@@ -9,6 +9,10 @@ public class Example_2 {
         System.out.println("Введите число: ");
         int num = in.nextInt();
 
+        //при делении на 5 в остатке получается 2,
+        // а при делении на 7 в остатке получается 1
+        //может подойти 22
+
         if(num%5 == 2 && num%7 == 1) {
             System.out.println("Число делится");
         } else {
